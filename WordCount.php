acd987ac8b05ba745f3e2cedf662f61c8b4ec4aa
Gretawdm <124893555+Gretawdm@users.noreply.github.com>
2023-10-20 22:15:30 +0700

    Update WordCount.php

--- WordCount.php
+++ WordCount.php
@@ -1,7 +1,13 @@
 <?php
+//deklarasi class
 class WordCount{
+    //deklarasi method dan parameter(digunakan untuk menghitung kata yang ada dalam kalimat yang akan diuji)
     public function countWords($sentence){
+        //explode digunakan untuk membagi kalimat yang diinputkan menjadi array
+        //explode juga berfungsi memisahkan kalimat berdasarkan spasi (" "), sehingga menghasilkan array kata-kata
+        //method count digunakan untuk menghitung jumlah elemen dalam array kata-kata tersebut. Jumlah ini akan menjadi jumlah kata dalam kalimat.
         return count(explode(" ", $sentence));
+        //return untuk mengembalikan hasil dari perhitungan kata
            
         }
     }
